fcf4272063c156838cd40774ec222468f3603586
ng2189-sowmya <80737663+ng2189-sowmya@users.noreply.github.com>
2024-05-20 20:40:44 +0530

    initial commit

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,5 +18,70 @@ describe 'String Calculator' do
         expect(string_calculator.add).to eq('0, 1, 6')
       end
     end
+
+    context 'with n number of numbers' do
+      it 'should return sum' do
+        string_calculator = StringCalculator.new('1,2,3,4', '1,5', '1,5,7')
+
+        expect(string_calculator.add).to eq('10, 6, 13')
+      end
+    end
+
+    context 'with new line character' do
+      it 'should return sum' do
+        string_calculator = StringCalculator.new('1\n2,3')
+
+        expect(string_calculator.add).to eq('6')
+      end
+    end
+
+    context 'with different delimiters character' do
+      it 'should return sum' do
+        string_calculator = StringCalculator.new('//;\n1;2')
+
+        expect(string_calculator.add).to eq('3')
+      end
+    end
+    
+    context 'with different delimiters character' do
+      it 'should return sum' do
+        string_calculator = StringCalculator.new('//;\n1;2')
+
+        expect(string_calculator.add).to eq('3')
+      end
+    end
+
+    context 'with negative numbers' do
+      it 'should return error message' do
+        string_calculator = StringCalculator.new('-1', '2,3,-4')
+
+        expect(string_calculator.add).to eq('Negative numbers not allowed - -1,-4')
+      end
+    end
+
+    context 'Numbers bigger than 1000' do
+      it 'should return error message' do
+        string_calculator = StringCalculator.new('1001, 2', '1,2')
+
+        expect(string_calculator.add).to eq('2, 3')
+      end
+    end
+
+    context 'With delimiters length' do
+      it 'should return error message' do
+        string_calculator = StringCalculator.new('//[***]\n1***2***3')
+
+        expect(string_calculator.add).to eq('6')
+      end
+    end
+
+    context 'With multiple delimiters length' do
+      it 'should return error message' do
+        string_calculator = StringCalculator.new('//[*][%]\n1*2%3')
+
+        expect(string_calculator.add).to eq('6')
+      end
+    end
+
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,17 +1,24 @@
 class StringCalculator
-
   def initialize(*numbers)
     @numbers = numbers
   end
 
   def add
     output = []
+    negative_numbers = []
+
     @numbers.each do |num|
-      array = num.split(/[^0-9]+/).map(&:to_i)
+      array = num.scan(/-?\d+/).map(&:to_i)
+      negative_numbers.concat(array.select { |a| a < 0 })
+      array.reject! { |x| x > 1000 }
+
+      next unless negative_numbers.empty?
+
       output << array.sum
     end
 
+    return "Negative numbers not allowed - #{negative_numbers.join(',')}" unless negative_numbers.empty?
+
     output.join(", ")
   end
-  
 end
